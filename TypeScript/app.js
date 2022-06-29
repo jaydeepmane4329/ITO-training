25041ec900dce17f25cef07ae3c843f994b31043
Jaydeep Mane <63183102+jaydeepmane4329@users.noreply.github.com>
2022-06-29 18:14:49 +0530

    Started with TypeScript...

---
+++ TypeScript/app.js
@@ -0,0 +1,46 @@
+// let message: string = 'Hello, TypeScript!';
+// let heading = document.createElement('h1');
+// heading.textContent = message;
+// document.body.appendChild(heading);
+// interface Product{
+//     id : number
+//     name : string
+//     price :number
+// }
+// function getProducts(id) : Product{
+//    return{
+//     id:id,
+//     name : `awesome Gadget ${id}`,
+//     price : 99.5,
+//    }
+// }
+// let product = getProducts(1);
+// console.log(product.name,console.log(product.price))
+// interface Product{
+//     id : number,
+//     name : string,
+//     price : number,
+// }
+// function getProducts(id) : Product{
+//  return{
+//      id : id,
+//      name : `Awesome Product ${id}`,
+//      price : 99.5,
+//  }
+// }
+// // let product = getProducts(1);
+// // console.log(`PRODUCT IS ${product.price} & costs $${product.name}`);
+// const showProduct =(name : string, price : number) => {
+//     console.log(`the Product ${name} costs ${price}$.`);
+// }
+// const product = getProducts(1);
+// showProduct( product.name,product.price,);
+// var myName = "jay";
+// console.log(myName);
+
+
+
+
+
+
+
---
+++ TypeScript/app.ts
@@ -0,0 +1,54 @@
+// let message: string = 'Hello, TypeScript!';
+
+// let heading = document.createElement('h1');
+// heading.textContent = message;
+
+// document.body.appendChild(heading);
+
+
+// interface Product{
+//     id : number
+//     name : string
+//     price :number
+// }
+
+// function getProducts(id) : Product{
+//    return{
+//     id:id,
+//     name : `awesome Gadget ${id}`,
+//     price : 99.5,
+//    }
+// }
+
+// let product = getProducts(1);
+// console.log(product.name,console.log(product.price))
+
+
+// interface Product{
+//     id : number,
+//     name : string,
+//     price : number,
+// }
+
+// function getProducts(id) : Product{
+//  return{
+//      id : id,
+//      name : `Awesome Product ${id}`,
+//      price : 99.5,
+//  }
+// }
+
+// // let product = getProducts(1);
+// // console.log(`PRODUCT IS ${product.price} & costs $${product.name}`);
+
+// const showProduct =(name : string, price : number) => {
+//     console.log(`the Product ${name} costs ${price}$.`);
+// }
+
+// const product = getProducts(1);
+// showProduct( product.name,product.price,);
+
+// let myName: string = "jay";
+// console.log(myName);
+
+
